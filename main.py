7fa919e08233eada0c1ed035e9472b3199993c1a
Gabriel G. R. Silva <gabrielgrsilva@outlook.com.br>
2025-07-01 12:36:57 -0300

    Add function to create a .txt file to track new medicine

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import medicine
+
 menu = """
 [t] Take pill
 [c] Check how many pills are left
 [a] Add more pills
+[n] New medicine
 [h] Help
 [q] Quit
 """
-print("========Welcome to Pill Counter!"========)
+print("========Welcome to Pill Counter!========")
 
 while True:
     print("Please select an option:")
@@ -19,7 +22,9 @@ while True:
 
     
     elif option == "c":
-        print("Which medicine count would you like to check?")
+        medicine_to_check = input("Which medicine count would you like to check?")
+        pills_left = medicine.Medicine.check_count(medicine_to_check)
+        print(f"You have {pills_left} pills left.")
         #asks for user input on the name of medicine, choosing from those that were added to their inventory
 
     elif option == "a":
@@ -27,11 +32,23 @@ while True:
         #asks for user input on the name of medicine, choosing from those that were added to their inventory
         print("How many pills do you want to add to your inventory?")
         #asks for a number input, adding the number to the total of pills from that medicine
+
+    elif option == "n":
+        # asks for input and creates a new instance of the medicine class
+        new_medicine = input("Please provide the name of the new medicine:")
+        time_to_take = input("At which time of day you will take this medicine? Provide just the number, like 15 for 15h: ")
+        #if time_to_take not int or > 24:           ADD ERROR MESSAGES LATER
+        #    print("Invalid time. Please start again providing the time of day you'll take the medicine")
+        starting_pills = input("Finally, how many pills you already have for this medicine? ")
+        #if starting_pills not int:
+        #    print("Invalid number of pills. Please type only numbers")
+        new_medicine = medicine.Medicine(new_medicine, time_to_take, starting_pills)
     
     elif option == "h":
         print("[t] Take pill -- Removes one pill from the total amount and marks the medicine as taken for today")
         print("[c] Check how many pills are left -- Allows you to see how many pills you have left from a certain medicine")
         print("[a] Add more pills -- Adds more pills to your total (for example, when you have bought more)")
+        print("[n] New medicine -- Adds a new medicine to be tracked by the app")
         print("[q] Quit - Exits Pill Counter")
 
     elif option == "q":
---
+++ medicine.py
@@ -0,0 +1,24 @@
+import os
+
+class Medicine:
+    def __init__(self, name, time, initial_count):
+        self.name = name
+        self.time = time
+        self.initial_count = initial_count
+        self.create_medicine_file()
+
+    def check_count(self):
+        file = open(f"data/{self.name}.txt", "r")
+        content = file.read() ##Reads the file, but how do I check exactly how many pills are left?
+
+    def create_medicine_file(self):         #this should create a new file to store info for a particular medicine
+        if os.path.exists(f"data/{self.name}.txt"):
+            print(f"========ERROR! This medicine is already being tracked!========")
+        else:
+            file = open(f"data/{self.name}.txt", "w")
+            file.write(f"Pills Left: {self.initial_count}\n")
+            file.write(f"Daily Time: {self.time}\n")
+            file.close()
+
+
+##    ; Remaining Pills; Daily Pills used; Time the medication is taken; If the pill was taken today or not yet;
